d3670f4c8a07cc9e29c417a0f0ffa6499a3f53e2
nvbn <nvbn.rm@gmail.com>
2014-03-06 00:16:06 +0400

    Fix error with per-view and not per-view csrf on one page

--- session_csrf/templatetags/session_csrf.py
+++ session_csrf/templatetags/session_csrf.py
@@ -1,4 +1,4 @@
-from copy import copy
+from contextlib import contextmanager
 from django.template.defaulttags import CsrfTokenNode
 from django import template
 from ..models import Token
@@ -7,15 +7,25 @@ from ..models import Token
 register = template.Library()
 
 
+@contextmanager
+def save_token(context):
+    is_exists = 'csrf_token' in context
+    token = context.get('csrf_token')
+    yield
+    if is_exists:
+        context['csrf_token'] = token
+    else:
+        del context['csrf_token']
+
 @register.simple_tag(takes_context=True)
 def per_view_csrf(context, view_name):
     """Register per view csrf token. Not pure!"""
-    _context = copy(context)
-    request = _context['request']
-    if request.user.is_authenticated():
-        token, _ = Token.objects.get_or_create(
-            owner=request.user, for_view=view_name)
-        _context['csrf_token'] = token.value
-    node = CsrfTokenNode()
-    return node.render(_context)
+    with save_token(context):
+        request = context['request']
+        if request.user.is_authenticated():
+            token, _ = Token.objects.get_or_create(
+                owner=request.user, for_view=view_name)
+            context['csrf_token'] = token.value
+        node = CsrfTokenNode()
+        return node.render(context)
 per_view_csrf.is_safe = True
